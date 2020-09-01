d4bb17294c09c6ee26acf631779ad683a284851a
Sipo Charles <sipocharles@yahoo.co.uk>
2020-09-01 15:29:31 +0100

    Building List and Detail Views

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
