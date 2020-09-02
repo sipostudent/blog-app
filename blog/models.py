7b8d056274fe39d55e1c2fc59894ac183431c38e
Sipo Charles <sipocharles@yahoo.co.uk>
2020-09-02 02:30:04 +0100

    Canonical URLs for Models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 class PublishedManager(models.Manager):
@@ -25,3 +26,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                                self.publish.month,
+                                self.publish.day, self.slug])
